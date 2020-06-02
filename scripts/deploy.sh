3d778bf63b96023afc72addb680cb284fabb86c5
itRect-01 <66125638+itRect-01@users.noreply.github.com>
2020-06-02 22:25:07 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+DEFAULT_BRANCH=$(curl 'https://api.github.com/repos/gh-un-it-rect/000-deploy' | jq '.default_branch' | tr -d \")
+echo "Default branch: ${DEFAULT_BRANCH}"
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo "This branch is the 'master' branch"
 else
- echo "This branch is the ${TRAVIS_BRANCH} branch"
+  echo "This branch is the ${TRAVIS_BRANCH} branch" 
+fi
  
