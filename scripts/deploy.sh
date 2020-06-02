b1b16e214b90ee2f54b90a62659d1ebd491d6e19
itRect-01 <66125638+itRect-01@users.noreply.github.com>
2020-06-02 23:54:57 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,7 +27,11 @@ function __profiler__ {
    echo ""
    
    echo " ------- TRAVIS -------"
-   echo "token: $__TOKEN_GITHUB__" 
+   echo "token: $__TOKEN_GITHUB__"
+   echo "arg: $__PREVIEW__" 
+   echo "arg: $__BODY_OK__"
+   echo "arg: $__REPO_DEPLOY_"
+   echo "arg: $__JSON__"
    echo "branch: $TRAVIS_BRANCH"
    echo " -------     -------"
   
@@ -36,6 +40,7 @@ function __profiler__ {
 function __execute__ {
    if [ "$TRAVIS_BRANCH" = "master" ]; then
      echo "This branch is the master branch"
+     curl PATCH -i -H $__PREVIEW__ -H $__JSON__ -H "Authorization: token $__TOKEN_GITHUB__" -d $__BODY_OK__ https://api.github.com/repos/$__REPO_DEPLOY_
      quit 0
    else
      echo -e "\e[31mPor favor recuerde que solo pueden deployar en rama Master\e[0m"
